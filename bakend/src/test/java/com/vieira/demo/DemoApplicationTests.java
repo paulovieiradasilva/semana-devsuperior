c7353bfe90b480ba7d3a2e5513edf51bea4c0597
Paulo Vieira da Silva <vieiraweb.tecnologia@gmail.com>
2020-09-14 18:46:45 -0300

    Project created

---
+++ bakend/src/test/java/com/vieira/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vieira.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
